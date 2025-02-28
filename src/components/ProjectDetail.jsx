4b3704163aaa5421f2dd92c4e07bab56b1a8aee3
Luis Curiel De Sousa <luisfrancisco.lfcds@outlook.com>
2025-02-28 13:30:36 -0500

    Add styling for Project Detail and Project Form

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,15 +1,12 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
 import { fetchProjectById } from "../services/projectService";
-import {
-    getCollaborators,
-    removeCollaborator,
-} from "../services/collaboratorService";
-import { useContext } from 'react';
-import AuthedUserContext from '../context/AuthedUserContext.js';
-import { jwtDecode } from "jwt-decode";
 import CollaboratorForm from "./CollaboratorForm";
 import ErrorMessage from "./ErrorMessage";
+import CollaboratorList from "./CollaboratorList.jsx";
+import { getCollaborators } from "../services/collaboratorService.js";
+import AuthedUserContext from "../context/AuthedUserContext.js";
+import { jwtDecode } from "jwt-decode";
 
 function ProjectDetail() {
     const { id } = useParams();
@@ -17,61 +14,80 @@ function ProjectDetail() {
     const [collaborators, setCollaborators] = useState([]);
     const [errorMessage, setErrorMessage] = useState("");
     const token = useContext(AuthedUserContext);
+    const user = jwtDecode(token).user_id;
+    const [currentCollaborator, setCurrentCollaborator] = useState(null);
 
     useEffect(() => {
         const loadProject = async () => {
-            const projectData = await fetchProjectById(id);
-            setProject(projectData);
-        };
-
-        const loadCollaborators = async () => {
-            const collaboratorData = await getCollaborators(id);
-            setCollaborators(collaboratorData);
-        };
-
-        loadProject();
-        loadCollaborators();
-    }, [id]);
-
-    const handleRemoveCollaborator = async collaboratorId => {
-        try {
-            const response = await removeCollaborator(collaboratorId);
-            if (response) {
-                setCollaborators((prev) => prev.filter(collaborator => collaborator.id !== collaboratorId));
+            try {
+                const projectData = await fetchProjectById(id);
+                setProject(projectData);
+            } catch (error) {
+                setErrorMessage("❌ Error fetching project:", error.message);
             }
-        } catch (error) {
-            setErrorMessage("❌ Error removing collaborator:", error.message);
+        };
+        
+        if (token) {
+            loadProject();
         }
-    };
+    }, [id, token, user]);
 
-    if (!project || !collaborators) return <p>Cargando...</p>;
+    useEffect(() => {
+        const fetchCollaborators = async () => {
+            try {
+                const collaboratorData = await getCollaborators(id);
+                setCurrentCollaborator(collaboratorData.find(collaborator => collaborator.user === user));
+                setCollaborators(collaboratorData)
+            } catch (error) {
+                setErrorMessage("❌ Error fetching collaborators:", error.message);
+            }
+        };
+
+        if (token) {
+            fetchCollaborators();
+        }
+    }, [id, token, user, collaborators]);
+
+
+    if (!project) return <p>Loading...</p>;
 
 
     if (!token) {
         console.error("🚨 No token.");
         return;
     }
-    const user = jwtDecode(token).user_id;
 
     return (
-        <div>
-            <h2>{project.name}</h2>
+        <div className="min-h-screen bg-gray-50 pt-22">
+            <div className="max-w-2xl mx-auto">
+                <div className="flex justify-between items-center mb-4">
+                    <h2 className="text-2xl font-bold">{project.name}</h2>
+                    <Link
+                        to={`/projects/${project.id}/edit`}
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                    >
+                        Go to Code Editor
+                    </Link>
+                </div>
+                <div className="bg-white p-4 rounded shadow-sm">
+                    <h3 className="text-lg font-semibold mb-4">Collaborators</h3>
 
-            <h3>Collaborators</h3>
-            {errorMessage && <ErrorMessage message={errorMessage} />}
-            <ul>
-                {collaborators.map(collaborator => (
-                    <li key={collaborator.user}>
-                        {collaborator.username} - {collaborator.role}
-                        {project.owner === user && collaborator.user !== user && (
-                            <button onClick={() => handleRemoveCollaborator(collaborator.id)}>
-                                Remove
-                            </button>
-                        )}
-                    </li>
-                ))}
-            </ul>
-            <CollaboratorForm projectId={id} />
+                    {errorMessage && <ErrorMessage message={errorMessage} />}
+
+                    <CollaboratorList
+                        currentCollaborator={currentCollaborator}
+                        collaborators={collaborators}
+                        setCollaborators={setCollaborators}
+                        owner={project.owner}
+                    />
+
+                    {currentCollaborator.role === 'admin' && <CollaboratorForm
+                        currentCollaborator={currentCollaborator}
+                        setCollaborators={setCollaborators}
+                        projectId={id}
+                    />}
+                </div>
+            </div>
         </div>
     );
 }
